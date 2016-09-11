e2ce4d77d07f9abf7b9248d6560ca5c77e515967
zhuhao1994 <18772101141@163.com>
2016-09-11 09:50:40 +0800

    弹出对话处理

--- app/src/main/java/zhu/com/ddclient/activity/LoginActivity.java
+++ app/src/main/java/zhu/com/ddclient/activity/LoginActivity.java
@@ -7,11 +7,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import zhu.com.ddclient.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private CustomDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +25,26 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+    public void dialogBtnClick(View v){
+        dialog = new CustomDialog(LoginActivity.this);
+        dialog.setTitle("配置选项");
+//        EditText editText = dialog.get
+        dialog.setOnPositiveListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //dosomething youself
+                String string = dialog.getSettingText();
+                Toast.makeText(getApplicationContext(), string,Toast.LENGTH_LONG).show();
+                dialog.dismiss();
 
+            }
+        });
+        dialog.setOnNegativeListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
 }
