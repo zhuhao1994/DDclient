74ee8cc2e1ca729f46e128b3c28e338bda877136
FishGold <18772101141@163.com>
2016-09-11 20:19:28 +0800

    222

--- app/src/main/java/zhu/com/ddclient/fragment/DetailFragment.java
+++ app/src/main/java/zhu/com/ddclient/fragment/DetailFragment.java
@@ -1,24 +1,23 @@
 package zhu.com.ddclient.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import zhu.com.ddclient.R;
 
 /**
  * Created by zhu on 2016/9/9.
  */
-public class CartFragment extends Fragment {
+public class DetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.cart_fragment,container,false);
+        View root = inflater.inflate(R.layout.deatil_fragment,container,false);
         return root;
+
     }
 }
