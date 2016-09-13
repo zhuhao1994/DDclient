d8b1873e576fef6ca8f00ac58ab42289888f7c14
FishGold <18772101141@163.com>
2016-09-13 16:27:27 +0800

    New changelist

--- app/src/main/java/zhu/com/ddclient/fragment/DetailFragment.java
+++ app/src/main/java/zhu/com/ddclient/fragment/DetailFragment.java
@@ -1,30 +1,44 @@
 package zhu.com.ddclient.fragment;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 import zhu.com.ddclient.R;
 import zhu.com.ddclient.activity.MainActivity;
 import zhu.com.ddclient.myinterface.ShowFragment;
 import zhu.com.ddclient.activity.MainActivity.ShowCommentsFragment;
+import zhu.com.ddclient.util.BitmapUtil;
+
 /**
  * Created by zhu on 2016/9/9.
  */
 public class DetailFragment extends Fragment {
     private ShowFragment  showComments =null;
     private Button detail_btn = null;
+    private JSONObject bookInfo = null; //书籍信息
     public void setShowComments(ShowCommentsFragment showComments){
         this.showComments = showComments;
+    }
+    public void setBookInfo(JSONObject bookInfo){
+        this.bookInfo = bookInfo;
     }
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.deatil_fragment,container,false);
+        initView(root);
         detail_btn = (Button)root.findViewById(R.id.detail_btn);
         detail_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -32,6 +46,47 @@ public class DetailFragment extends Fragment {
             }
         });
         return root;
+    }
+    //初始化
+    public void initView(View root){
+        TextView bookNameTV = (TextView) root.findViewById(R.id.bookName);
+        TextView priceTV = (TextView) root.findViewById(R.id.price);
+        RatingBar startnumRB = (RatingBar) root.findViewById(R.id.starnum);
+        TextView authorTV = (TextView) root.findViewById(R.id.author);
+        TextView publisherTV = (TextView) root.findViewById(R.id.publisher);
+        TextView stockstatusTV = (TextView) root.findViewById(R.id.stockstatus);
+        TextView sellVolumeTV = (TextView) root.findViewById(R.id.sellVolume);
+        ImageView bookImgIV = (ImageView)root.findViewById(R.id.bookImg);
+        try {
+            bookNameTV.setText(bookInfo.getString("bookName"));
+            priceTV.setText(bookInfo.getString("price"));
+            startnumRB.setRating(bookInfo.getInt("starnum"));
+            authorTV.setText(bookInfo.getString("author"));
+            publisherTV.setText(bookInfo.getString("publisher"));
+            stockstatusTV.setText(bookInfo.getString("stockstatus"));
+            sellVolumeTV.setText(bookInfo.getString("salesVolume"));
+
+            String  url = BitmapUtil.BASE_URL+bookInfo.getString("imagePath")+bookInfo.getString("imageName");
+            asynsetImage(url,bookImgIV);
+        }catch (Exception e){
+
+        }
+
+    }
+
+    //异步加载图片资源
+    public void asynsetImage(String url,final ImageView iv){
+        new AsyncTask<String, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(String... params) {
+                return BitmapUtil.getHttpBitmap(params[0]);
+            }
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                super.onPostExecute(bitmap);
+                iv.setImageBitmap(bitmap);
+            }
+        }.execute(url);
     }
 
 }
--- app/src/main/java/zhu/com/ddclient/fragment/HomeFragment.java
+++ app/src/main/java/zhu/com/ddclient/fragment/HomeFragment.java
@@ -1,18 +1,33 @@
 package zhu.com.ddclient.fragment;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import zhu.com.ddclient.R;
 import zhu.com.ddclient.activity.MainActivity.ShowDetailFragment;
+import zhu.com.ddclient.util.BitmapUtil;
+import zhu.com.ddclient.util.HttpUtil;
 import zhu.com.ddclient.myinterface.ShowFragment;
 
 /**
@@ -23,6 +38,7 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemClickLis
     private Context context;
     private BooKListAdapter adapter = null;
     private ShowFragment showDetail = null;
+    private JSONArray serverData = null ;//存储服务器数据
     public void setShowDetail(ShowDetailFragment showDetail){
         this.showDetail = showDetail;
     }
@@ -33,25 +49,59 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemClickLis
         super.onCreate(savedInstanceState);
     }
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,Bundle savedInstanceState) {
+
+        requestServerData("");  //向服务器请求书籍列表
         View root = inflater.inflate(R.layout.fragment_home,container,false);
-        adapter = new BooKListAdapter();
         lv = (ListView) root.findViewById(R.id.listView);
-        lv.setAdapter(adapter);
         lv.setOnItemClickListener(this);
+        adapter = new BooKListAdapter();
+        lv.setAdapter(adapter);
         return root;
     }
 
     //监听listview
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-          showDetail.show();
+        try {
+            JSONObject bookinfo = (JSONObject) serverData.get(position);
+            showDetail.show(bookinfo);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+    //请求服务数据 参数 bookName
+    protected void requestServerData(String bookName){
+        Map<String,String> params = new HashMap<>();
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        try {
+            jsonObject.put("name",bookName);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        jsonArray.put(jsonObject);
+        params.put("params",jsonObject.toString());
+        try {
+            String s = HttpUtil.postRequest("http://172.19.22.17:8080/books.json",params);
+            Log.i("服务器数据: ",s);
+            serverData =  new JSONArray(s);
+            Log.i("长度: ",serverData.length()+"");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     class BooKListAdapter extends BaseAdapter {
         public int getCount() {
-            return 10;
+            return serverData.length();
         }
         public Object getItem(int position) {
-            return 1;
+            try {
+                return serverData.get(position);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
         }
         public long getItemId(int position) {
             return position;
@@ -59,7 +109,35 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemClickLis
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = LayoutInflater.from(context);
             LinearLayout lineLayout = (LinearLayout) inflater.inflate(R.layout.book_item,null);
+            TextView bookNameTV = (TextView) lineLayout.findViewById(R.id.bookName);
+            TextView priceTV = (TextView) lineLayout.findViewById(R.id.price);
+            RatingBar starNumTV = (RatingBar) lineLayout.findViewById(R.id.starnum);
+            ImageView bookImgIV = (ImageView) lineLayout.findViewById(R.id.bookImg);
+            try {
+                JSONObject bookInfo = (JSONObject) serverData.get(position);
+                String  url = BitmapUtil.BASE_URL+bookInfo.getString("imagePath")+bookInfo.getString("imageName");
+                asynsetImage(url,bookImgIV);
+                bookNameTV.setText(bookInfo.getString("bookName"));
+                priceTV.setText(bookInfo.getString("price"));
+                starNumTV.setRating(bookInfo.getInt("starnum"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             return lineLayout;
+        }
+        //异步加载图片资源
+        public void asynsetImage(String url,final ImageView iv){
+            new AsyncTask<String, Void, Bitmap>() {
+                @Override
+                protected Bitmap doInBackground(String... params) {
+                   return BitmapUtil.getHttpBitmap(params[0]);
+                }
+                @Override
+                protected void onPostExecute(Bitmap bitmap) {
+                    super.onPostExecute(bitmap);
+                    iv.setImageBitmap(bitmap);
+                }
+            }.execute(url);
         }
     }
 
