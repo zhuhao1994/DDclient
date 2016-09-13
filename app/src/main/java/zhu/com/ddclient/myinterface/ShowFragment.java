c1995b9d6d318775d6be9e6f39ebc01ff26c4ffa
FishGold <18772101141@163.com>
2016-09-13 16:30:12 +0800

    New changelist

--- app/src/main/java/zhu/com/ddclient/myinterface/ShowFragment.java
+++ app/src/main/java/zhu/com/ddclient/myinterface/ShowFragment.java
@@ -1,8 +1,11 @@
 package zhu.com.ddclient.myinterface;
 
+import org.json.JSONObject;
+
 /**
  * Created by zhu on 2016/9/12.
  */
 public  interface ShowFragment {
     void show();
+    void show(JSONObject s);
 }
